c3050ca7d09ec779b7a279e28666e3827e2e41a2
sumisumi2000 <sumio.vantan308@gmail.com>
2024-01-31 07:39:47 +0900

    2月のテンプレートを作成

--- Template/makeTemplate.rb
+++ Template/makeTemplate.rb
@@ -1,18 +1,21 @@
+# coding: utf-8
+
 # 年をここに入力
 Year = 2024
 # 月をここに入力
-Month = 1
+Month = 2
 # 月の日数を入力
-maxDay = 31
+maxDay = 29
 BigDayOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 DayOfTheWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
 # 月の初めの曜日を入力、日曜日を0とする
-firstDayOfTheWeek = 1
+firstDayOfTheWeek = 4
 
 # 月の初めの曜日が2番目の要素になるように並び替え
 ((firstDayOfTheWeek + 6) % 7).times do
   # 先頭から要素を削除し、末尾に追加
   DayOfTheWeek.push(DayOfTheWeek.shift)
+  BigDayOfTheWeek.push(BigDayOfTheWeek.shift)
 end
 
 puts "# #{Year}/#{format("%02d", Month)}"
